6ef9de3edb728c5216e6b2e5296b271321270715
ccdu1010 <ccdu1010@gmail.com>
2023-08-17 15:20:14 +0100

    Task7: Create endpoint postCommentForArticle update2

--- errors/index.js
+++ errors/index.js
@@ -4,7 +4,7 @@ exports.handleCustomErrors = (err, req, res, next) => {
     } else next(err);
 };
 
-exports.handlePsqlErrors = (err, req, res, next) => 
+exports.handlePsqlErrors = (err, req, res, next) => {
     const invalid_text_representation = '22P02';
     const foreign_key_violation = '23503';
     if (err.code === foreign_key_violation || err.code === invalid_text_representation) {
